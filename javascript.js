befc8cd0501c04f8d007cc78ecb49a6c97e60f1c
jd-loper <joedanlop@yahoo.com>
2022-07-01 12:18:57 -0700

    Create function for test

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
 const rpsArray = ["Rock!", "Paper!", "Scissors!"];
 
+const userInput = prompt("Rock, paper, or scissors?");
+
 const randomValue = Math.floor(Math.random() * rpsArray.length);
 console.log(rpsArray[randomValue]);
+
+function rock() {
+    if (userInput == "rock" && randomValue == 0) {
+        return console.log("It's a tie!");
+    } else if (userInput == "rock" && randomValue == 2) {
+        return console.log("You win! Rock beats scissors.");
+    } else {
+        return console.log("You lose. Paper beats rock.");
+    }
+}
+
+rock();
