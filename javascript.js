a27cfb8c4d3eef3637a812e231654d122f6f6df2
Joseph Loper <jd.loper@icloud.com>
2024-04-23 23:50:52 -0700

    Add maximum for rounds, convert logs to DOM elements

--- javascript.js
+++ javascript.js
@@ -1,77 +1,91 @@
 let win = 0;
 let loss = 0;
 let tie = 0;
+let roundCount = 0;
+let maxRounds = 5;
 
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const scissorsBtn = document.querySelector("#scissorsBtn");
+const resultsDiv = document.querySelector("#results");
 
-playRound.addEventListener("click", function () {
-  for (i = 0; i < 5; i++) {
-    let userChoice = prompt("Rock, paper, or scissors?").toUpperCase();
-    if (!userChoice) {
-      console.log("That's not how this game works!");
-      loss += 1;
-    } else {
-      console.log("Player:" + " " + userChoice);
-    }
+rockBtn.addEventListener("click", () => playRound("ROCK"));
+paperBtn.addEventListener("click", () => playRound("PAPER"));
+scissorsBtn.addEventListener("click", () => playRound("SCISSORS"));
 
-    let computerChoice = Math.ceil(Math.random() * 3);
-    if (computerChoice == 1) {
-      computerChoice = "ROCK";
-    } else if (computerChoice == 2) {
-      computerChoice = "PAPER";
-    } else {
-      computerChoice = "SCISSORS";
-    }
-
-    console.log("Computer:" + " " + computerChoice);
-    let compare = function (choice1, choice2) {
-      if (choice1 === choice2) {
-        tie += 1;
-        return "It's a tie!";
-      }
-      if (choice1 === "ROCK") {
-        if (choice2 === "SCISSORS") {
-          win += 1;
-          return "You win!";
-        } else {
-          loss += 1;
-          return "You lose! Try again.";
-        }
-      }
-      if (choice1 === "PAPER") {
-        if (choice2 === "ROCK") {
-          win += 1;
-          return "You win!";
-        } else {
-          loss += 1;
-          return "You lose! Try again.";
-        }
-      }
-      if (choice1 === "SCISSORS") {
-        if (choice2 === "ROCK") {
-          loss += 1;
-          return "You lose! Try again.";
-        } else {
-          win += 1;
-          return "You win!";
-        }
-      }
-    };
-    let results = compare(userChoice, computerChoice);
-    console.log("Results: " + results);
+function playRound(userChoice) {
+  if (roundCount >= maxRounds) {
+    finalResult();
+    return;
   }
-});
 
-console.log("Final Results: W-" + win + "; L-" + loss + "; T-" + tie + ";");
-let finalResult = function (win, loss) {
-  if (win >= 3) {
-    console.log("You win the game!");
-  } else if (loss >= 3) {
-    console.log("You lose the game...");
+  const computerChoice = getComputerChoice();
+  const result = compare(userChoice, computerChoice);
+
+  resultsDiv.textContent = `\nRound ${
+    roundCount + 1
+  }: Player: ${userChoice} | Computer: ${computerChoice} | ${result}`;
+
+  roundCount++;
+
+  if (win >= 3 || loss >= 3) {
+    finalResult();
+  }
+}
+
+function getComputerChoice() {
+  let computerChoice = Math.ceil(Math.random() * 3);
+  if (computerChoice == 1) {
+    return "ROCK";
+  } else if (computerChoice == 2) {
+    return "PAPER";
   } else {
-    console.log("Stalemate! Try again.");
+    return "SCISSORS";
   }
-};
-finalResult(win, loss);
+}
+
+function compare(choice1, choice2) {
+  if (choice1 === choice2) {
+    tie += 1;
+    return "It's a tie!";
+  }
+  if (choice1 === "ROCK") {
+    if (choice2 === "SCISSORS") {
+      win += 1;
+      return "You win!";
+    } else {
+      loss += 1;
+      return "You lose! Try again.";
+    }
+  }
+  if (choice1 === "PAPER") {
+    if (choice2 === "ROCK") {
+      win += 1;
+      return "You win!";
+    } else {
+      loss += 1;
+      return "You lose! Try again.";
+    }
+  }
+  if (choice1 === "SCISSORS") {
+    if (choice2 === "ROCK") {
+      loss += 1;
+      return "You lose! Try again.";
+    } else {
+      win += 1;
+      return "You win!";
+    }
+  }
+}
+
+function finalResult() {
+  let finalMessage;
+  if (win >= 3) {
+    finalMessage = "Congratulations, you win the game!";
+  } else if (loss >= 3) {
+    finalMessage = "You lose the game...";
+  } else {
+    finalMessage = "Stalemate!";
+  }
+  resultsDiv.innerHTML += `<br>${finalMessage}`;
+}
